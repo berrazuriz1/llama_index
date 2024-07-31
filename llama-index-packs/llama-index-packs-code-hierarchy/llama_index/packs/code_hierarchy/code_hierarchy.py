e06c51b2cf93d4cedbfac65bdb4b72d719618e6d
José Domínguez <jndominguez@uc.cl>
2024-07-31 11:16:46 -0700

    added arrow functions to TS

--- llama-index-packs/llama-index-packs-code-hierarchy/llama_index/packs/code_hierarchy/code_hierarchy.py
+++ llama-index-packs/llama-index-packs-code-hierarchy/llama_index/packs/code_hierarchy/code_hierarchy.py
@@ -192,7 +192,8 @@ class _ChunkNodeOutput(BaseModel):
 
 
 class CodeHierarchyNodeParser(NodeParser):
-    """Split code using a AST parser.
+    """
+    Split code using a AST parser.
 
     Add metadata about the scope of the code block and relationships between
     code blocks.
@@ -324,6 +325,36 @@ class CodeHierarchyNodeParser(NodeParser):
             end_byte = node.end_byte
         return text[start_byte:end_byte].strip()
 
+    def has_arrow_function(self, parent_node: BaseNode) -> bool:
+
+        try:
+            import tree_sitter_languages
+        except ImportError:
+            raise ImportError(
+                "Please install tree_sitter_languages to use CodeSplitter."
+            )
+
+        query = """
+        (lexical_declaration
+            (variable_declarator
+                value: (arrow_function)
+            ) @var
+        )
+        """
+        language = tree_sitter_languages.get_language(self.language)
+
+        arrow_function_query = language.query(query)
+        inheritances_captures = arrow_function_query.captures(parent_node)
+        return bool(inheritances_captures)
+
+    def check_arrow_function(self, parent_node: Node) -> bool:
+        if (
+            self.language in ["jsx", "tsx", "typescript", "javascript"]
+            and parent_node.type == "lexical_declaration"
+        ):
+            return bool(self.has_arrow_function(parent_node))
+        return True
+
     def _chunk_node(
         self,
         parent: Node,
@@ -372,7 +403,12 @@ class CodeHierarchyNodeParser(NodeParser):
         # TIP: This is a wonderful place to put a debug breakpoint when
         #      Trying to integrate a new language. Pay attention to parent.type to learn
         #      all the available node types and their hierarchy.
-        if parent.type in self.signature_identifiers or _root:
+        if (
+            parent.type in self.signature_identifiers
+            and self.check_arrow_function(parent)
+            or _root
+        ):
+
             # Get the new context
             if not _root:
                 new_context = _ScopeItem(
@@ -433,9 +469,9 @@ class CodeHierarchyNodeParser(NodeParser):
                             ].append(  # type: ignore
                                 d.as_related_node_info()
                             )
-                            d.relationships[
-                                NodeRelationship.PARENT
-                            ] = this_document.as_related_node_info()
+                            d.relationships[NodeRelationship.PARENT] = (
+                                this_document.as_related_node_info()
+                            )
                 # Otherwise we pass the children upstream
                 else:
                     # If we have been given a document, that means it's
@@ -587,9 +623,9 @@ class CodeHierarchyNodeParser(NodeParser):
                         assert parent_tag in tag_to_type
                         parent_type = tag_to_type[parent_tag]
                         if parent_type not in self.signature_identifiers:
-                            self.signature_identifiers[
-                                parent_type
-                            ] = _SignatureCaptureOptions(name_identifier=_node.type)
+                            self.signature_identifiers[parent_type] = (
+                                _SignatureCaptureOptions(name_identifier=_node.type)
+                            )
 
             if (
                 not tree.root_node.children
@@ -607,9 +643,9 @@ class CodeHierarchyNodeParser(NodeParser):
                         **chunk.metadata,
                         **node.metadata,
                     }
-                    chunk.relationships[
-                        NodeRelationship.SOURCE
-                    ] = node.as_related_node_info()
+                    chunk.relationships[NodeRelationship.SOURCE] = (
+                        node.as_related_node_info()
+                    )
 
                 if self.skeleton:
                     self._skeletonize_list(chunks)
@@ -664,9 +700,9 @@ class CodeHierarchyNodeParser(NodeParser):
                                         new_split_nodes[0].as_related_node_info()
                                     )
                                 new_children.append(old_nodes_child)
-                            old_node.relationships[
-                                NodeRelationship.CHILD
-                            ] = new_children
+                            old_node.relationships[NodeRelationship.CHILD] = (
+                                new_children
+                            )
 
                             # Handle parent node
                             if (
@@ -674,9 +710,9 @@ class CodeHierarchyNodeParser(NodeParser):
                                 and old_node.parent_node.node_id
                                 == original_node.node_id
                             ):
-                                old_node.relationships[
-                                    NodeRelationship.PARENT
-                                ] = new_split_nodes[0].as_related_node_info()
+                                old_node.relationships[NodeRelationship.PARENT] = (
+                                    new_split_nodes[0].as_related_node_info()
+                                )
 
                         # Now save new_nodes_
                         new_nodes += new_split_nodes
